8f5f14a14e77dc7295130a3cbb9cdaf4096a4e9f
Ida <ida.berg2003@gmail.com>
2025-03-18 20:30:36 +0100

    Add project files.

---
+++ ASP.NET_Assignment/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_Assignment.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
